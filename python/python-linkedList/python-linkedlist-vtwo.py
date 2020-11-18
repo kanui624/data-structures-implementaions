d66680904d62e0e0ea040848e6da0c42eb990e7d
Kanui <marley.kanui@gmail.com>
2020-11-17 22:23:42 -0600

    data-stuctures - global commit 21 - added list class

--- python/python-linkedList/python-linkedlist-vtwo.py
+++ python/python-linkedList/python-linkedlist-vtwo.py
@@ -2,3 +2,8 @@ class Node:
     def __init__(self, input=None):
         self.input = input
         self.next = None
+
+class SinglyList:
+    def __init__(self):
+        self.head = None
+        
