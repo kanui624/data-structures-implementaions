2a1136e120c5e73a03bdd2c17fbeda7708b5f1bf
Kanui <marley.kanui@gmail.com>
2020-11-17 22:30:20 -0600

    data-stuctures - global commit 23 - added printList method

--- python/python-linkedList/python-linkedlist-vtwo.py
+++ python/python-linkedList/python-linkedlist-vtwo.py
@@ -7,10 +7,17 @@ class SinglyList:
     def __init__(self):
         self.head = None
 
+    def printList(self):
+        printinput = self.head
+        while printinput is not None:
+            print (printinput.input)
+            printinput = printinput.next
+
 
 list = SinglyList()
 list.head = Node("hey")
 two = Node("yo")
-three = Node("Wed")
+three = Node("hello")
 list.head.next = two
 two.next = three
+list.printList()
