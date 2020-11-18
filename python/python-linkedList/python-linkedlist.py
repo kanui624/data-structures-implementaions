e0ed90c181f1632c263703899b6337509609a368
Kanui <marley.kanui@gmail.com>
2020-11-17 22:13:56 -0600

    data-stuctures - global commit 17 - Added insertNode method

--- python/python-linkedList/python-linkedlist.py
+++ python/python-linkedList/python-linkedlist.py
@@ -7,6 +7,17 @@ class LinkedList:
     def __init__(self):
         self.head = None
 
+    def insertNode(self, data):
+        newNode = Node(data)
+        if(self.head):
+            current = self.head
+            while(current.next):
+                current = current.next
+            current.next = newNode
+        else:
+            self.head = newNode
+
 List = LinkedList()
 List.head = Node(3)
-print(List.head.data)
+List.insertNode(5)
+print(List)
