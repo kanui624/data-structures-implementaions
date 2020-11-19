ccf9a1fc1e604ced4761be25139b8c0b5f499462
Kanui <marley.kanui@gmail.com>
2020-11-18 21:46:15 -0600

    data-stuctures - global commit 27 - Added insertAtHead method

--- javascript/javascript-linkedlist/javascript-linkedlist.js
+++ javascript/javascript-linkedlist/javascript-linkedlist.js
@@ -8,6 +8,22 @@ class Node {
 class SinglyLinkedList {
   constructor() {
     this.head = null;
-    this.tail = tail;
+    this.nodeCount = 0;
+  }
+
+  insertAtHead(input) {
+    let newNode = new Node(input);
+    if (!this.head) {
+      this.head = newNode;
+    } else {
+      newNode.nextNode = this.head;
+      this.head = newNode;
+    }
+    this.nodeCount++;
   }
 }
+
+const list = new SinglyLinkedList();
+list.insertAtHead(10);
+list.insertAtHead(12);
+console.log(list);
