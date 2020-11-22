9874977734ee6fd228844fdf8d187670ae342687
Kanui <marley.kanui@gmail.com>
2020-11-21 21:59:52 -0600

    data-stuctures - global commit 40 - Added Linked List class and listed out required methods

--- javascript/javascript-linkedlist/javascript-singlylinkedlist-two.js
+++ javascript/javascript-linkedlist/javascript-singlylinkedlist-two.js
@@ -4,3 +4,20 @@ class Node {
     this.nextNode = nextNode;
   }
 }
+
+class LinkedList {
+  constructor() {
+    this.head = head;
+    this.nodeCount = 0;
+  }
+}
+
+// insert
+// insertAtHead
+// insertAtTail
+// getIndexHelper
+// insertAtIndex
+// removeHead
+// removeTail
+// removeAtIndex
+// printList
