73c72f486f6f8a3b9f25a5bad70bbd7d38d418c5
Kanui <marley.kanui@gmail.com>
2020-11-23 00:46:21 -0600

    data-stuctures - python linkedlist implementation two - global commit 62 - Implemented Linked List class

--- python/python-linkedList/python-singlylinkedlist-two.py
+++ python/python-linkedList/python-singlylinkedlist-two.py
@@ -1,4 +1,11 @@
 class Node:
-    def __init__(self, node, nextNode = null):
+    def __init__(self, node, nextNode = None):
         self.node = node
         self.nextNode = nextNode
+
+class LinkedList: 
+    def __init__(self):
+        self.head = None
+        self.nodeCount = 0
+
+    
