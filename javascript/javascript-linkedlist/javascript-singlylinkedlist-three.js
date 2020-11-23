ac9dbf70e24070fe7097d01beee8361848090e4d
Kanui <marley.kanui@gmail.com>
2020-11-22 22:02:33 -0600

    data-stuctures - implementation three - global commit 54 - Implemented insert at head Method

--- javascript/javascript-linkedlist/javascript-singlylinkedlist-three.js
+++ javascript/javascript-linkedlist/javascript-singlylinkedlist-three.js
@@ -11,6 +11,17 @@ class SinglyLinkedList {
     this.nodeCount = 0;
   }
 
+  insertAtHead(input) {
+    let newHeadNode = new Node(input);
+    if (!this.head) {
+      this.head = newHeadNode;
+    } else {
+      newHeadNode.nextNode = this.head;
+      this.head = newHeadNode;
+    }
+    this.nodeCount++;
+  }
+
   printList() {
     return JSON.stringify(this.head, null, 4);
   }
@@ -24,5 +35,8 @@ class SinglyLinkedList {
 // getIndexHelper
 
 const list = new SinglyLinkedList();
+list.insertAtHead(1);
+list.insertAtHead(2);
+list.insertAtHead(3);
 const printedList = list.printList();
 console.log(printedList);
