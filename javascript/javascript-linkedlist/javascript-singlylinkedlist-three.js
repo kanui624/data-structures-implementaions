672531528cd5cb92df119ad0e16c9fb5bf8e93e8
Kanui <marley.kanui@gmail.com>
2020-11-23 00:03:01 -0600

    data-stuctures - implementation three - global commit 57 - Implemented insertAtTail method

--- javascript/javascript-linkedlist/javascript-singlylinkedlist-three.js
+++ javascript/javascript-linkedlist/javascript-singlylinkedlist-three.js
@@ -22,6 +22,16 @@ class SinglyLinkedList {
     this.nodeCount++;
   }
 
+  insertAtTail(input) {
+    let newTailNode = new Node(input);
+    let prevTailNode = this.getNodeAtIndex(this.nodeCount - 1);
+    if (!this.head) {
+      this.head = newTailNode;
+    }
+    prevTailNode.nextNode = newTailNode;
+    this.nodeCount++;
+  }
+
   getNodeAtIndex(index) {
     if (index < 0 || index > this.nodeCount - 1) {
       return { error: `Index: ${index} is out of range` };
@@ -54,6 +64,8 @@ const list = new SinglyLinkedList();
 list.insertAtHead(1);
 list.insertAtHead(2);
 list.insertAtHead(3);
+list.insertAtTail(4);
 const printedList = list.printList();
-const getNode = list.getNodeAtIndex(0);
-console.log(printedList, getNode, `NodeCount: ${list.nodeCount}`);
+
+// const getNode = list.getNodeAtIndex(0);
+console.log(printedList, `NodeCount: ${list.nodeCount}`);
