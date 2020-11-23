1364913e584079e955a66d75a45cff91e7f8f7f3
Kanui <marley.kanui@gmail.com>
2020-11-23 10:36:43 -0600

    data-stuctures - python linkedlist implementation two - global commit 69 - Implemented getIdexHelper method

--- python/python-linkedList/python-singlylinkedlist-two.py
+++ python/python-linkedList/python-singlylinkedlist-two.py
@@ -29,15 +29,17 @@ class LinkedList:
             self.nodeCount += 1
 
     def getIndexHelper(self, index):
-        if(index == 0):
+        if(index < 0 or index > self.nodeCount - 1):
+            print("Index: {} is out of Range".format(index))
+        elif(index == 0):
             print(self.head.node)
         else: 
             counterNode = self.head
-            while(counterNode):
+            counter = 0
+            while(counter != index):
                 counterNode = counterNode.nextNode
-            print(counterNode.node) 
-
-
+                counter += 1
+            print(counterNode.node)
 
     def printList(self):
         current = self.head
@@ -53,7 +55,7 @@ list.insertAtHead(1)
 list.insertAtHead(2)
 list.insertAtHead(3)
 list.insertAtTail(7)
-list.getIndexHelper(2)
+# list.getIndexHelper(4)
 list.printList()
 # insertAtIndex
 
