3621e43253b174bfd03107d8a5de205233f947a6
Kanui <marley.kanui@gmail.com>
2020-11-29 22:50:13 -0600

    data-structures - Javascript linked-list implementation - global commit 85 - Wrote tasks to complete in comments

---
+++ javascript/javascript-linkedlist/javascript-singlylinkedlist-six.js
@@ -0,0 +1,13 @@
+// Implement a Linked List
+
+// Tasks:
+// Implement Node and Linked List class
+// Implement all necessary Methods for a singly linked list
+// insertAtHead
+// insertAtTail
+// insertAtIndex
+// getIndexHelper
+// removeHead
+// removeTail
+// removeAtIndex
+// printList
