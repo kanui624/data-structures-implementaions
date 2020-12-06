0fdf1a469e0e9b7034c53185c5d06655d99f77d8
Kanui <marley.kanui@gmail.com>
2020-12-06 16:55:07 -0600

    linked-list-implementations - global commit 621 - Added insertAtHead and printList methods - Javascript Implementation

--- javascript/javascript-linkedlist/javascript-singlylinkedlist-nine.js
+++ javascript/javascript-linkedlist/javascript-singlylinkedlist-nine.js
@@ -10,7 +10,31 @@ class SinglyLinkedList {
     this.head = null;
     this.nodeCount = 0;
   }
+
+  insertAtHead(input) {
+    let newNodeAtHead = new Node(input);
+    if (!this.head) {
+      this.head = newNodeAtHead;
+    } else {
+      newNodeAtHead.nextNode = this.head;
+      this.head = newNodeAtHead;
+    }
+    this.nodeCount++;
+  }
+
+  printList() {
+    console.log(
+      JSON.stringify(this.head, null, 4),
+      `Node Count: ${this.nodeCount}`
+    );
+  }
 }
+
+let list = new SinglyLinkedList();
+list.insertAtHead(1);
+list.insertAtHead(2);
+list.insertAtHead(3);
+list.printList();
 
 // insertAtHead
 // insertAtTail
