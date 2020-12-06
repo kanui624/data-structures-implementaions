1fc327ea9168c544b5e548b3e2b2e10740eb30ca
Kanui <marley.kanui@gmail.com>
2020-12-06 16:58:10 -0600

    linked-list-implementations - global commit 63 - Added getAtIndex method - Javascript Implementation

--- javascript/javascript-linkedlist/javascript-singlylinkedlist-nine.js
+++ javascript/javascript-linkedlist/javascript-singlylinkedlist-nine.js
@@ -22,6 +22,22 @@ class SinglyLinkedList {
     this.nodeCount++;
   }
 
+  getIndexHelper(index) {
+    if (index < 0 || index > this.nodeCount) {
+      console.log(`Index: ${index} is out of range`);
+    } else if (index === 0) {
+      return this.head;
+    } else {
+      let current = this.head;
+      let count = 0;
+      while (count !== index) {
+        current = current.nextNode;
+        count++;
+      }
+      return current;
+    }
+  }
+
   printList() {
     console.log(
       JSON.stringify(this.head, null, 4),
