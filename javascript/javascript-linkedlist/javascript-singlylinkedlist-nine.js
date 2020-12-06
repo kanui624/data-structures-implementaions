b1a3f1b402ae382c921a2bca9b248fb7876fcf14
Kanui <marley.kanui@gmail.com>
2020-12-06 17:00:49 -0600

    linked-list-implementations - global commit 64 - Added insertAtTail method - Javascript Implementation

--- javascript/javascript-linkedlist/javascript-singlylinkedlist-nine.js
+++ javascript/javascript-linkedlist/javascript-singlylinkedlist-nine.js
@@ -22,6 +22,17 @@ class SinglyLinkedList {
     this.nodeCount++;
   }
 
+  insertAtTail(input) {
+    let newTailNode = new Node(input);
+    if (!this.head) {
+      this.head = newTailNode;
+    } else {
+      let currentTail = this.getIndexHelper(this.nodeCount - 1);
+      currentTail.nextNode = newTailNode;
+    }
+    this.nodeCount++;
+  }
+
   getIndexHelper(index) {
     if (index < 0 || index > this.nodeCount) {
       console.log(`Index: ${index} is out of range`);
@@ -50,6 +61,7 @@ let list = new SinglyLinkedList();
 list.insertAtHead(1);
 list.insertAtHead(2);
 list.insertAtHead(3);
+list.insertAtTail(6);
 list.printList();
 
 // insertAtHead
