976c1027b0eadff76f7fa92203ef2875910f7279
Kanui <marley.kanui@gmail.com>
2020-12-26 23:40:01 -0600

    linked-list-implementations - global commit 170  - Added insertAtHead Method - Javascript Implementation 12

--- javascript/javascript-linkedlist/javascript-singlylinkedlist-twelve.js
+++ javascript/javascript-linkedlist/javascript-singlylinkedlist-twelve.js
@@ -11,6 +11,17 @@ class SinglyLinkedList {
     this.nodeCount = 0;
   }
 
+  insertAtHead(input) {
+    let newHeadNode = new Node(input);
+    if (!this.head) {
+      this.head = newHeadNode;
+    } else {
+      newHeadNode.nextNode = this.head;
+      this.head = newHeadNode;
+    }
+    this.nodeCount++;
+  }
+
   printList() {
     console.log(
       JSON.stringify(this.head, null, 4),
